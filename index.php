a32c0d15b942f473d48b5327223914e41824118c
dylaya <50320641+dylaya@users.noreply.github.com>
2019-05-13 14:58:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <html>
  <head>
  <Title>Registration Form</Title>
+ <link rel="stylesheet" href="./material.min.css">
+ <script src="./material.min.js"></script>
+ 
  <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
+ 	/*body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
  	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
  	}
@@ -12,7 +15,7 @@
  	h3 { font-size: 1.2em; }
  	table { margin-top: 0.75em; }
  	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+ 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }*/
  </style>
  </head>
  <body>
